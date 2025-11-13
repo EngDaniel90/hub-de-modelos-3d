4159eb0a966871abc010221a4d0b91a433d24559
EngDaniel90 <daniel.anversi@gmail.com>
2025-11-14 00:08:44 +0800

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,16 @@
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open('app-cache').then(cache => cache.addAll([
+      './',
+      './index.html',
+      './links.json',
+      './images/'
+    ]))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
